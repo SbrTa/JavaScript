6f4578d53a861a9b79c9897c9b902c4a1dd02211
Subrata Roy <subrataxon@gmail.com>
2019-03-10 17:15:40 +0600

    ok

---
+++ Vue/Practice/01-Component-Advanced/src/components/new.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+        <h2>New</h2>
+        <button @click="counter++">Increase</button>
+        <p>Counter = {{counter}}</p>
+    </div>
+</template>
+
+<script>
+    export default {
+        data: function () {
+            return{
+                counter:0
+            }
+        }
+    }
+</script>
+
+<style >
+
+</style>
