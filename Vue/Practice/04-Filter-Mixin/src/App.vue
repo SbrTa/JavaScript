5e248ad9e97305dacba4cc7dcf01155d84effd48
Subrata Roy <subrataxon@gmail.com>
2019-03-14 16:33:49 +0600

    ok

--- Vue/Practice/04-Filter-Mixin/src/App.vue
+++ Vue/Practice/04-Filter-Mixin/src/App.vue
@@ -1,25 +1,40 @@
 <template>
-  <div id="app">
+  <div class="container">
     <h1>Filters & Mixins</h1>
     <hr>
     <p>{{text | toLowerCase}}</p>
     <p>{{text | toUpperCase}}</p>
     <p>{{text | toLowerCase | toUpperCase}}</p>
+    <hr>
+
+    <input v-model="filterText"/>
+    <ul>
+      <li v-for="fruit in filteredFruits"> {{fruit}} </li>
+    </ul>
+
+    <app-list></app-list>
   </div>
 </template>
 
 <script>
-export default {
+  import List from './List.vue';
+  import {fruitMixin} from "./fruitMixin";
+
+  export default {
   data(){
     return {
-      text : 'Sample Text'
+      text : 'Sample Text',
     }
   },
   filters:{
     toLowerCase(value){
       return value.toLowerCase();
     }
-  }
+  },
+  components:{
+    appList: List
+  },
+  mixins: [fruitMixin]
 
 }
 </script>
