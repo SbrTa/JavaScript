63d9c5f0810d33c1904f58957a962e9245f8c3c6
Subrata Roy <subrataxon@gmail.com>
2019-03-21 14:53:53 +0600

    ok

--- Vue/Practice/05-HTTP/src/main.js
+++ Vue/Practice/05-HTTP/src/main.js
@@ -7,6 +7,20 @@ Vue.use(VueResource);
 Vue.http.options.root = 'https://vuejs-http-sbrta.firebaseio.com/data.json';
 
 
+Vue.http.interceptors.push((request, next) => {
+  console.log(request);
+  if(request.method == 'POST'){
+    request.method = 'PUT';
+  }
+  next(response => {
+    response.json = ()=>{
+      return {
+        message : response.body
+      }
+    }
+  });
+})
+
 
 new Vue({
   el: '#app',
