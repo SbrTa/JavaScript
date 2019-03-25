98855027ec1c1cbb666f5b06289ea3a84f749020
Subrata Roy <subrataxon@gmail.com>
2019-03-25 19:26:02 +0600

    ok

---
+++ Vue/Practice/06-Routing/src/components/user/UserDetail.vue
@@ -0,0 +1,12 @@
+<template>
+    <div>
+        <h3>Some User Details</h3>
+        <hr>
+        <p>Loaded User: {{$route.params.id}}</p>
+
+        <router-link
+                tag="button"
+                :to="{name: 'userEdit', params: {id: $route.params.id}}"
+                class="btn btn-primary">Edit</router-link>
+    </div>
+</template>
