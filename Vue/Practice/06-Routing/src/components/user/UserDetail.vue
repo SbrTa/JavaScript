44ecdaa219f33969c9f22282115173c96bea0d03
Subrata Roy <subrataxon@gmail.com>
2019-03-27 12:41:27 +0600

    ok

--- Vue/Practice/06-Routing/src/components/user/UserDetail.vue
+++ Vue/Practice/06-Routing/src/components/user/UserDetail.vue
@@ -6,7 +6,27 @@
 
         <router-link
                 tag="button"
-                :to="{name: 'userEdit', params: {id: $route.params.id}}"
+                :to="link"
                 class="btn btn-primary">Edit</router-link>
     </div>
 </template>
+
+<script>
+    export default {
+        data(){
+            return{
+                link:{
+                    name: 'userEdit',
+                    params: {
+                        id: this.$route.params.id
+                    },
+                    query: {
+                        local: 'EN',
+                        q: 100
+                    },
+                    hash: '#data'
+                }
+            }
+        }
+    }
+</script>
--- Vue/Practice/06-Routing/src/components/user/UserEdit.vue
+++ Vue/Practice/06-Routing/src/components/user/UserEdit.vue
@@ -1,3 +1,12 @@
 <template>
-    <h3>Edit the User</h3>
+    <div>
+        <h3>Edit the User</h3>
+        <p>Local : {{$route.query.local}}</p>
+        <p>Q : {{$route.query.q}}</p>
+
+        <div style="height: 1000px">
+
+        </div>
+        <p id="data">Scroll to here</p>
+    </div>
 </template>
