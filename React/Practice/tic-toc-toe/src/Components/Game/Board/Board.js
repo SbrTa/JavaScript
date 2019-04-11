478edce182091ef61978d7f302b15d5e5c2ab44d
Subrata Roy <subrataxon@gmail.com>
2019-04-11 19:37:50 +0600

    tic-toc-toe

--- React/Practice/tic-toc-toe/src/Components/Game/Board/Board.js
+++ React/Practice/tic-toc-toe/src/Components/Game/Board/Board.js
@@ -1,33 +1,72 @@
 import React from 'react'
 import './Board.css'
 import Square from './Square/Square'
+import {findWinner} from './Winner'
 
 class Board extends React.Component {
 
+    constructor(props) {
+        super(props);
+        this.state = {
+            squares: Array(9).fill(null),
+            xNow: true
+        }
+    }
+
+    handleClick(i){
+        const squares = this.state.squares.slice();
+        
+        if(findWinner(squares) || squares[i]){
+            return;
+        }
+
+        squares[i] = this.state.xNow ? 'X' : 'O';
+        this.setState({
+            squares,
+            xNow: !this.state.xNow
+        })
+    }
+
+
     renderSquare(i){
-        return <Square value={i}></Square>
+        return <Square 
+                    value={this.state.squares[i]}
+                    onClick={()=>this.handleClick(i)}>
+                </Square>
     }
 
 
     render() {
+        const winner = findWinner(this.state.squares);
+        let status = null;
+        if(winner){
+            status = 'Winner: ' + winner;
+        }else{
+            status = 'Next player : ' + (this.state.xNow ? 'X':'O');
+        }
+
         return (
             <div>
-                <div className="board-row">
-                    {this.renderSquare(1)}
-                    {this.renderSquare(2)}
-                    {this.renderSquare(3)}
-                </div>
-                <div className="board-row">
-                    {this.renderSquare(4)}
-                    {this.renderSquare(5)}
-                    {this.renderSquare(6)}
-                </div>
-                <div className="board-row">
-                    {this.renderSquare(7)}
-                    {this.renderSquare(8)}
-                    {this.renderSquare(9)}
+                <div className="status">{status}</div>
+                <div>
+                    <div className="board-row">
+                        {this.renderSquare(0)}
+                        {this.renderSquare(1)}
+                        {this.renderSquare(2)}
+                    </div>
+                    <div className="board-row">
+                        {this.renderSquare(3)}
+                        {this.renderSquare(4)}
+                        {this.renderSquare(5)}
+                    </div>
+                    <div className="board-row">
+                        {this.renderSquare(6)}
+                        {this.renderSquare(7)}
+                        {this.renderSquare(8)}
+                    </div>
                 </div>
             </div>
+            
         )
     }
 }
