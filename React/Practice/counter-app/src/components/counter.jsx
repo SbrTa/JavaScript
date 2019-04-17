4afbd3b938af5cc6f0c9cbcd3a4a24d4d3d732d9
Subrata Roy <subrataxon@gmail.com>
2019-04-17 14:25:32 +0600

    ok

---
+++ React/Practice/counter-app/src/components/counter.jsx
@@ -0,0 +1,36 @@
+import React, { Component } from "react";
+
+class Counter extends Component {
+  state = {
+    value: this.props.value
+  };
+
+  formatCount() {
+    return this.state.value === 0 ? "zero" : this.state.value;
+  }
+
+  getBadgeClasses() {
+    let classes = "badge m-2 badge-";
+    classes += this.state.value === 0 ? "warning" : "primary";
+    return classes;
+  }
+
+  handleIncrement = () => {
+    this.setState({
+      value: this.state.value + 1
+    });
+  };
+
+  render() {
+    return (
+      <div>
+        <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
+        <button onClick={this.handleIncrement} className="btn btn-secondary">
+          Increment
+        </button>
+      </div>
+    );
+  }
+}
+
+export default Counter;
